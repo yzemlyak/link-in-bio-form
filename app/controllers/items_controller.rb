03df082f0d370847a7603c016cc8810f2861c20c
yzemlyak <yzemlyak@gmail.com>
2025-05-06 05:18:07 +0000

    First part

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,8 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def display_form
+    render({:template => "item_templates/backdoor_form"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-
+  get("/backdoor", {:controller => "items", :action => "display_form"})
   get("/", { :controller => "items", :action => "index" })
+
 end
