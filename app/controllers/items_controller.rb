c34318d58f5050a8ce66e35e3db670f25eee0ff7
yzemlyak <yzemlyak@gmail.com>
2025-05-06 05:30:30 +0000

    some progress

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,17 @@ class ItemsController < ApplicationController
   def display_form
     render({:template => "item_templates/backdoor_form"})
   end
+
+  def create_item
+
+
+
+    i = Item.new 
+    i.link_url = params.fetch("link_url_param")
+    i.link_description = params.fetch("link_description_param")
+    i.thumbnail_url = params.fetch("thumbnail_url_param")
+    i.save
+    
+    render({:template => "item_templates/create"})
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get("/backdoor", {:controller => "items", :action => "display_form"})
+  
+  post("/make_a_new_item", {:controller => "items", :action => "create_item"})
+  
   get("/", { :controller => "items", :action => "index" })
 
 end
